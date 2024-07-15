6766bd01dd4624a0d8548197f7772a43fca91bdb
Mayas Hasan <mayas.hasan@infostrategic.com>
2024-07-15 10:55:42 +0300

    .

--- dist/songs/songs.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class SongsService {
-    private readonly songs;
-    create(song: any): any;
-    findAll(): any[];
-    findOne(id: any): void;
-    update(id: any): any;
-    delete(): string;
-}
